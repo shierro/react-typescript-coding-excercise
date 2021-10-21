5a3899194786a5696c41a21f8174324d6e358cd7
Theo <theodoro.pelingan@gmail.com>
2021-10-21 17:04:17 +0800

    :hammer: migrate app into a container, to prepare for scale

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="header p-4 flex flex-row items-center border-b-2 bg-gray-50">
       <img src={logo} width="80" className="max-w-screen-sm animate-spin-slow" alt="React Logo" />
-      <h1 className="text-2xl font-serif">Toptal React Coding Challenge</h1>
+      <h1 className="text-2xl font-serif">React Coding Exercises</h1>
     </header>
   );
 }
--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '.';
 
 test('renders the basic structure', () => {
   render(<App />);
   const bodyElement = screen.getByRole('main');
-  const headerElement = screen.getByText('Toptal Coding Challenge');
+  const headerElement = screen.getByText('React Coding Exercises');
   const footerElement = screen.getByText('Baked with Love');
   expect(bodyElement).toBeInTheDocument();
   expect(footerElement).toBeInTheDocument();
